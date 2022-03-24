d6775f976a8a1fe4f392e2e3b9fbfdbeafc7c910
jo3l17 <joelvaldezangeles@gmail.com>
2022-03-23 23:24:52 -0500

    fix: url enviroment fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://lyj-wedding.herokuapp.com/',
+  apiUrl: 'https://lyj-wedding.herokuapp.com',
   googleClientId: '334315013698-i1cvlgnpjstudcp3ie86v40cm9dpuokq.apps.googleusercontent.com',
   facebookClientId: '2146991385452349'
 };
