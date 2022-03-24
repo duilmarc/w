b893998bd93bc4ecb13114111cb680aae8660489
jo3l17 <joelvaldezangeles@gmail.com>
2022-03-23 22:39:51 -0500

    feat: added production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://lyj-wedding.herokuapp.com/',
   googleClientId: '334315013698-i1cvlgnpjstudcp3ie86v40cm9dpuokq.apps.googleusercontent.com',
   facebookClientId: '2146991385452349'
 };
