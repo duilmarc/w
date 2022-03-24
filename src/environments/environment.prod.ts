6ab8a5a88edcb44ea893bd0de6b3c6fca534bda3
jo3l17 <joelvaldezangeles@gmail.com>
2022-03-23 23:53:19 -0500

    feat: added new credential

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://lyj-wedding.herokuapp.com',
-  googleClientId: '334315013698-i1cvlgnpjstudcp3ie86v40cm9dpuokq.apps.googleusercontent.com',
+  googleClientId: '334315013698-k6vdf3gh07djl3e95c865cirttermghq.apps.googleusercontent.com',
   facebookClientId: '2146991385452349'
 };
