b34017ec44eb63728d2b198edd040124177f61a1
Franco Chavez <franco.chp@gmail.com>
2024-07-29 21:15:47 -0500

    tweak: solve a issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://lucero-franco-wedding-f663cce6b432.herokuapp.com/m',
+  apiUrl: 'https://lucero-franco-wedding-f663cce6b432.herokuapp.com/',
   jwtSecret: 'secret',
   googleClientId: '334315013698-k6vdf3gh07djl3e95c865cirttermghq.apps.googleusercontent.com',
   facebookClientId: '2146991385452349'
