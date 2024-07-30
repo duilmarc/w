fe7a57682428d81d2ec8c9745707fd0f7a38923e
Franco Chavez <franco.chp@gmail.com>
2024-07-29 22:36:14 -0500

    lucero-franco-wedding

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// Get dependencies
 const express = require("express");
 const path = require("path");
 const http = require("http");
@@ -5,7 +6,6 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const dotenv = require("dotenv");
-const cors = require("cors");
 
 dotenv.config();
 
@@ -14,15 +14,8 @@ const routes = require("./routes");
 
 const app = express(); // create an instance of express
 
-// Configure CORS to allow requests from specific origins
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-
 // Tell express to use the following parsers for POST data
 app
-  .use(cors(corsOptions)) // Add support for CORS
   .use(bodyParser.json())
   .use(
     bodyParser.urlencoded({
@@ -31,21 +24,33 @@ app
   )
   .use(cookieParser())
   .use(logger("dev")) // Tell express to use the Morgan logger
+  // Add support for CORS
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
+    next();
+  })
   // Tell express to use the specified director as the
   // root directory for your web site
   .use(express.static(path.join(__dirname, "../dist/lucero-franco-wedding")));
 
 // Tell express to map the default route ('/') to the index route
 // app.use("/", index);
+
+// ... ADD YOUR CODE TO MAP YOUR URL'S TO ROUTING FILES HERE ...
 app.use("/api", routes);
 
 // Tell express to map all other non-defined routes back to the index page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/lucero-franco-wedding/index.html"));
 });
-
-// Handle preflight requests for all routes
-app.options("*", cors(corsOptions));
 
 // Define the port address and tell express to use this port
 const PORT = process.env.PORT || "3000";
